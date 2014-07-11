65485f28acf2f2e1eb779d5a6eccbd28d84a8eb8
fgassert <cowbox314@gmail.com>
2014-07-11 05:14:35 -0400

    updated server setup scripts

--- app/wsgi.py
+++ app/wsgi.py
@@ -8,6 +8,8 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
 import os
+import sys
+sys.path.insert(0,os.path.join(os.path.dirname(__file__),'..'))
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "app.settings")
 
 from django.core.wsgi import get_wsgi_application
--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -6,36 +6,37 @@ sudo add-apt-repository -y ppa:ubuntugis/ppa
 sudo apt-get update -qq
 
 sudo apt-get install -y gdal-bin libgdal-dev python-gdal
-sudo apt-get install -y pip
+sudo apt-get install -y python-pip
 sudo pip install virtualenv
-virtualenv TESTING
-source TESTING/bin/activate
 
 sudo apt-get install -y apache2-mpm-worker apache2-dev
-wget http://modwsgi.googlecode.com/files/mod_wsgi-3.4.tar.gz
-tar xvfz mod_wsgi-3.4.tar.gz
-cd mod_wsgi-3.4
-./configure
-sudo make
-sudo make install
-echo "LoadModule wsgi_module /usr/lib/apache2/modules/mod_wsgi.so" > /etc/apache2/mods-available/wsgi.load
+sudo apt-get install -y valibapache2-mod-wsgi
+sudo apt-get install -y p7zip
 sudo a2enmod wsgi
 sudo a2dissite default
 
 cd /srv/
+sudo virtualenv TESTING
+source TESTING/bin/activate
+
 sudo git clone https://github.com/fgassert/watershed_app.git
 sudo pip install -r watershed_app/requirements-dev.txt
 sudo pip install -r watershed_app/requirements.txt
 
+
 sudo useradd --system --no-create-home --home-dir /srv/watershed_app --user-group DJANGO_APP
 sudo chsh -s /bin/bash DJANGO_APP
 
-wget https://raw.github.com/fgassert/watershed_app/master/apache_conf
-sudo mv apache_conf /etc/apache2/sites-available/md.cc
+sudo mkdir watershed_app/logs
+sudo wget http://md.cc.s3.amazonaws.com/tmp/assets.7z
+sudo p7zip -d assets.7z
+sudo mv assets /srv/watershed_app/
+
+sudo mv watershed_app/apache_conf /etc/apache2/sites-available/md.cc
 
 sudo a2ensite md.cc
 
-chown -R DJANGO_APP:DJANGO_APP /srv/watershed_app/
+sudo chown -R DJANGO_APP:DJANGO_APP /srv/watershed_app/
 
-service apache2 restart
+sudo service apache2 restart
 
