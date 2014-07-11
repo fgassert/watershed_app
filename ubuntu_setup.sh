4b6f8b55932ab6928ae8b5479dd27f0753041cc3
fgassert <cowbox314@gmail.com>
2014-07-11 08:49:12 -0400

    setup

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -2,7 +2,8 @@
 
 sudo apt-get install -y git
 sudo apt-get install -y gcc g++
-sudo apt-get install -y python python-dev python-software-properties
+sudo apt-get install -y python 
+sudo apt-get install python-dev python-software-properties
 sudo add-apt-repository -y ppa:ubuntugis/ppa
 sudo apt-get update -qq
 sudo apt-get install -y p7zip
@@ -10,17 +11,19 @@ sudo apt-get install -y gdal-bin libgdal-dev python-gdal
 sudo apt-get install -y python-pip
 sudo pip install virtualenv
 
-sudo apt-get install -y apache2-mpm-worker apache2-dev
+sudo apt-get install -y apache2-mpm-worker apache2-dev apache2-utils
 sudo apt-get install -y libapache2-mod-wsgi
-#echo "LoadModule wsgi_module /usr/lib/apache2/modules/mod_wsgi.so" > wsgi.load
-sudo mv wsgi.load /etc/apache2/mods-available/
 sudo a2enmod wsgi
+sudo a2enmod headers
 sudo a2dissite default
 
 cd /srv/
 sudo virtualenv TESTING
 source TESTING/bin/activate
 
+
+export C_INCLUDE_PATH=/usr/include/gdal
+export CPLUS_INCLUDE_PATH=/usr/include/gdal
 sudo git clone https://github.com/fgassert/watershed_app.git
 sudo pip install -r watershed_app/requirements-dev.txt
 sudo pip install -r watershed_app/requirements.txt
@@ -34,7 +37,7 @@ sudo wget http://md.cc.s3.amazonaws.com/tmp/assets.7z
 sudo p7zip -d assets.7z
 sudo mv assets /srv/watershed_app/
 
-sudo mv watershed_app/apache_conf /etc/apache2/sites-available/md.cc
+sudo mv watershed_app/apache_conf /etc/apache2/sites-available/md.cc.conf
 
 sudo a2ensite md.cc
 
